8a7929c6cba582563d008ae34e89887af89efb0d
camy0414 <camygarcia2402@gmail.com>
2026-05-12 17:29:45 -0500

    Creación ruta inventario.
    Se creo la ruta para que desde el dashboard vaya a inventario.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Dashboard } from './dashboard/dashboard';
 import { Usuarios } from './usuarios/usuarios';
+import { Inventario } from './inventario/inventario';
 
 export const routes: Routes = [
   { path: '', component: Login },
   { path: 'dashboard', component: Dashboard },
-  { path: 'usuarios', component: Usuarios }
+  { path: 'usuarios', component: Usuarios },
+  {path: 'inventario', component: Inventario}
 ];
