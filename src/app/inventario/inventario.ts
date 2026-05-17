9dbe6f58c8e7d71df4ab228c3a844833253d6de7
camy0414 <camygarcia2402@gmail.com>
2026-05-17 08:52:51 -0500

    Creación ventana emergente
    para crear producto o eliminar o agregar cantidades

--- src/app/inventario/inventario.ts
+++ src/app/inventario/inventario.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-inventario',
   standalone: true,
-  imports: [RouterLink, FormsModule],
+  imports: [RouterLink, FormsModule, CommonModule],
   templateUrl: './inventario.html',
   styleUrls: ['./inventario.css']
 })
 export class Inventario {
 
+  mostrarCrear = false;
+  mostrarCantidad = false;
+
+  abrirCrear(){
+    this.mostrarCrear = true;
+  }
+
+  abrirCantidad(){
+    this.mostrarCantidad = true;
+  }
+
+  cerrarModales(){
+    this.mostrarCrear = false;
+    this.mostrarCantidad = false;
+  }
+  validarCantidad(cantidad:number){
+
+  if(cantidad <= 0){
+    alert("La cantidad debe ser mayor a 0");
+    return false;
+  }
+
+  return true;
+
+  }
 }
