77c61633550057de6e985c3c8ab12c61f7e8be0e
camy0414 <camygarcia2402@gmail.com>
2026-05-17 07:50:16 -0500

    Botones inventario
    Agregue los botones para producto y para cantidad

--- src/app/inventario/inventario.ts
+++ src/app/inventario/inventario.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
+
 @Component({
   selector: 'app-inventario',
-  imports: [RouterLink, BrowserModule, FormsModule],
-
+  standalone: true,
+  imports: [RouterLink, FormsModule],
   templateUrl: './inventario.html',
-  styleUrl: './inventario.css',
-
-
+  styleUrls: ['./inventario.css']
 })
 export class Inventario {
 
