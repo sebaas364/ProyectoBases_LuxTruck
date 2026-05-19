5280b8ce83ad741f2c9951e44556c0992f37dc89
Sebastian Perez <sebasph2006@gmail.com>
2026-05-18 21:51:02 -0500

    maquina create

--- backLuxtruck/src/main/java/co/edu/unbosque/backLuxtruck/repository/MaquinaRepository.java
+++ backLuxtruck/src/main/java/co/edu/unbosque/backLuxtruck/repository/MaquinaRepository.java
@@ -1,12 +1,19 @@
 package co.edu.unbosque.backLuxtruck.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import co.edu.unbosque.backLuxtruck.model.Maquina;
+import java.util.List;
+
 
 public interface MaquinaRepository extends CrudRepository<Maquina, Integer> {
 	boolean existsByNumeroSerie(String numeroSerie);
 	boolean existsByTipo(String tipo);
+	
+	Optional<Maquina> findByNumeroSerie(String numeroSerie);
+	Optional<Maquina> findByIdMaquina(Integer idMaquina);
 	
 	long countByEstadoMaquinaEstado(String estado);
 }
