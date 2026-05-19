539dae8c45f74d48f0a4c0cf73f7c59e84a35432
Sebastian Perez <sebasph2006@gmail.com>
2026-05-18 22:28:23 -0500

    Last create enpoint fixed

--- backLuxtruck/src/main/java/co/edu/unbosque/backLuxtruck/repository/ClientePersonaRepository.java
+++ backLuxtruck/src/main/java/co/edu/unbosque/backLuxtruck/repository/ClientePersonaRepository.java
@@ -1,9 +1,17 @@
 package co.edu.unbosque.backLuxtruck.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import co.edu.unbosque.backLuxtruck.model.ClientePersona;
 
 public interface ClientePersonaRepository extends CrudRepository<ClientePersona, Integer> {
 
+	Optional<ClientePersona> findByNumeroDocumento(String numeroDocumento);
+
+	Optional<ClientePersona> findByIdPersona(Integer idPersona);
+	
+
+	Optional<ClientePersona> findByCorreo(String correo);
 }
